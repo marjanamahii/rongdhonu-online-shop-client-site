af24a2fa720429716264cea362fddf2c30ec04ab
marjanamahii <marjanamai777@gmail.com>
2022-02-26 21:57:04 +0600

    footer updated

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -13,15 +13,15 @@ const Footer = () => {
                         <div className="row">
                             <div className="col-lg-3 col-sm-6">
                                 <div className="address">
-                                    <h3 className="text-uppercase fw-bold text-white">in2travel.inc</h3>
+                                    <h3 className="text-uppercase fw-bold text-white">rongdhonu.inc</h3>
                                     <p className="mb-4 mt-4">
                                         <FontAwesomeIcon icon={faMapMarkerAlt} /> Dhaka, Bangladesh, 1206.
                                     </p>
                                     <p>
-                                        <FontAwesomeIcon icon={faPhoneAlt} /> 01743-389153 (10AM-5PM)
+                                        <FontAwesomeIcon icon={faPhoneAlt} /> 01710101011 (10AM-5PM)
                                     </p>
                                     <p>
-                                        <FontAwesomeIcon icon={faEnvelope} /> in2travel87@gmail.com
+                                        <FontAwesomeIcon icon={faEnvelope} /> rongdhonushop@gmail.com
                                     </p>
                                     <div className="my-5">
                                         <img src="https://i.ibb.co/2tWssgC/google-play.png" alt="" />
@@ -72,9 +72,9 @@ const Footer = () => {
                         <div className="row">
                             <div className="col-12">
                                 <p >
-                                    Copyright &copy; <span id="currentYear"></span> Yellow Devs | Site designed by <a
+                                    Copyright &copy; <span id="currentYear"></span> Rongdhonu Shop | Site designed by <a
                                         href="https://yellow-devs.netlify.app/" target="_blank" rel="noreferrer" title="Yellow Devs Website">
-                                        <span className="text-white"  >Yellow Devs</span></a>
+                                        <span className="text-white"  >Rongdhonu Online Shop</span></a>
                                 </p>
                             </div>
                         </div>
